cfee6ec37996f627e1154241e3456d353fed2a24
DVSHKMR <ashudevesh654@gmail.com>
2022-06-29 16:50:51 +0530

    checking Website

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5>Hello I am</h5>
         <h1>Devesh Kumar</h1>
         <h5 className="text-light">Front End Developer</h5>
         <CTA />
